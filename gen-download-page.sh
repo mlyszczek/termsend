cc306f52f5347762729ca63cbba89d3f9e469324
Michał Łyszczek <michal.lyszczek@bofc.pl>
2018-01-29 17:37:08 +0100

    fix: bug in site generation
    no_buildbot

--- gen-download-page.sh
+++ gen-download-page.sh
@@ -6,7 +6,7 @@ remote="http://distfiles.kurwinet.pl/${project}"
 
 # extract links to files
 links="$(curl "${remote}/" 2>/dev/null \
-    | grep "${project}-[0-9]*\.[0-9]*\.[0-9]*\.")"
+    | grep "${project}-[0-9]*\.[0-9]*\.[0-9]*\." | sort -r)"
 
 exec 1<&-
 exec 1<>"${out}"
@@ -24,5 +24,5 @@ echo "<h2>tarballs</h2>"
 echo "<pre>"
 
 # convert links to absolute
-echo "${links}" | sed "s:a href=\":a href=\"${remote_sed}/:"
+echo "${links}" | sed "s!a href=\"!a href=\"${remote}/!"
 echo "</pre>"
