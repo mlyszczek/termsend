d792ab4530c9adc1a52a51d595768553d2e3bbaa
Michał Łyszczek <michal.lyszczek@bofc.pl>
2018-10-12 23:03:58 +0200

    fix: logs are not shown right after startup
    sync log every time something is printed

--- src/main.c
+++ src/main.c
@@ -127,6 +127,7 @@ int main(int argc, char *argv[])
     el_option(EL_TS_TM, EL_TS_TM_REALTIME);
     el_option(EL_FINFO, 1);
     el_option(EL_COLORS, g_config.colorful_output);
+    el_option(EL_FILE_SYNC_EVERY, 0);
 
     if (el_option(EL_FPATH, g_config.program_log) != 0)
     {
