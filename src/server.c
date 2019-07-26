d4d93baa7382250ad91865f16c0deba9418bd5a5
Michał Łyszczek <michal.lyszczek@bofc.pl>
2019-07-26 11:57:48 +0200

    src/server.c: fix potential buffer overflow error
    url is created with config_domain_name and generated filename, but
    url was of size 1024 and config_domain_name was 4096. This could
    lead to buffer overflow when config_domain_name was big enough.
    
    Reported-by: pvs-studio
    Signed-off-by: Michał Łyszczek <michal.lyszczek@bofc.pl>

--- src/server.c
+++ src/server.c
@@ -396,7 +396,7 @@ static void *server_handle_upload
     int                 opathlen;    /* length of output directory path */
     char                path[PATH_MAX];  /* full path to the file */
     char                fname[32];   /* random generated file name */
-    char                url[1024];   /* generated link to uploaded data */
+    char                url[8192 + 1];   /* generated link to uploaded data */
     char                ends[8 + 1]; /* buffer for end string detection */
     static int          flen = 5;    /* length of the filename to generate */
     unsigned char       buf[8192];   /* temp buffer we read uploaded data to */
